5ba17f13bcd6b86401ba7431d2757f8ac3943387
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-02-28 01:24:07 +0300

    display view

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Photographer(models.Model):
+    name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=10)
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+class Image(models.Model):
+    title = models.CharField(max_length =30)
+    photographer = models.ForeignKey(Photographer)
+    description = models.TextField(max_length =30)
+    image = models.ImageField(upload_to = 'photos/', default='No image')
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True) 
+
