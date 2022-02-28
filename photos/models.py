0f28727848764a28cadc1e5cbdf972be16cc9cce
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-01 02:48:23 +0300

    add image model methods

--- photos/models.py
+++ photos/models.py
@@ -64,3 +64,25 @@ class Image(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,)
     pub_date = models.DateTimeField(auto_now_add=True, null=True) 
 
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title    
+    classmethod
+    def get_all_images(cls):
+        all_images = Image.objects.all()
+        return all_images
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        an_image = Image.objects.get(id=id)
+        return an_image    
+
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        photo = cls.objects.filter(category__category__icontains=search_term)
+        return photo
