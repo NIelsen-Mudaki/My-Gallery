8a0f20f5597b909e98cf87301a0f3ed0fff7372f
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-02 01:56:23 +0300

    add images

--- gallery/settings.py
+++ gallery/settings.py
@@ -123,7 +123,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
